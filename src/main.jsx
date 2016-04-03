7ff35973d44fb50b837ed73600a2263d3d3699e1
Jeffrey Gietz <jeffgietz@gmail.com>
2016-04-03 22:18:14 +1000

    React Seed

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var List = require('./components/List.jsx');
+console.log('here');
+
+ReactDOM.render(<List />, document.getElementById('ingredients'));
